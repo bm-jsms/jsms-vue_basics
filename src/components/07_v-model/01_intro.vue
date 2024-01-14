d3e612583dde8112e03d9aca5750a0a2fcf1a928
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-14 11:19:57 -0500

    07 - v-model

---
+++ src/components/07_v-model/01_intro.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+const formData = ref({
+	username: '',
+	email: '',
+	password: '',
+});
+</script>
+<template>
+	<form>
+		<input type="text" placeholder="username" v-model="formData.username" />
+		<input type="email" placeholder="email" v-model="formData.email" />
+		<input type="password" placeholder="password" v-model="formData.password" />
+	</form>
+	<ul>
+		<li>username: {{ formData.username }}</li>
+		<li>email: {{ formData.email }}</li>
+		<li>password: {{ formData.password }}</li>
+	</ul>
+</template>
+<style scoped></style>
