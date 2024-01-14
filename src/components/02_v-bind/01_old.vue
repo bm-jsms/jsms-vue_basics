567dfdd8273834f0c38852376fe05f096b3b3d81
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-14 02:16:07 -0500

    02 _ v-bind

---
+++ src/components/02_v-bind/01_old.vue
@@ -0,0 +1,7 @@
+<script setup>
+const myLink = 'https://github.com';
+</script>
+
+<template>
+	<h2>Go to GitHub: <a v-bind:href="myLink">GitHub</a></h2>
+</template>
---
+++ src/components/02_v-bind/02_new.vue
@@ -0,0 +1,22 @@
+<script setup>
+const myLink = 'https://github.com';
+const imageLink =
+	'https://images.unsplash.com/photo-1561037404-61cd46aa615b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+const imageAlt = 'a beautiful puppy';
+const imageWidth = 300;
+const imageHeight = 200;
+</script>
+
+<template>
+	<h2>Go to GitHub: <a :href="myLink">GitHub</a></h2>
+	<hr />
+	<h2>
+		{{ imageAlt }}
+	</h2>
+	<img
+		:src="imageLink"
+		:alt="imageAlt"
+		:width="imageWidth"
+		:height="imageHeight"
+	/>
+</template>
