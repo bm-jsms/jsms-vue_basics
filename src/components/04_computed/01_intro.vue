ad50517fc22b457943cdc9ea8718189e026d7887
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-14 10:06:21 -0500

    04 - Computed property

---
+++ src/components/04_computed/01_intro.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref, computed } from 'vue';
+const number = ref(5);
+const square = computed(() => number.value * number.value);
+const increment = () => number.value++;
+</script>
+<template>
+	<h1>Original Number: {{ number }}</h1>
+	<h1>Square: {{ square }}</h1>
+	<button @click="increment">Increment</button>
+</template>
+<style scoped></style>
