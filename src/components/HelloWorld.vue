3082968d26fbd14184fe75385a6c4d5aa12c95e2
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-13 23:56:22 -0500

    init vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<template>
+	<h1>Hello World!</h1>
+</template>
+<style scoped>
+h1 {
+	color: #970303;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import { createApp } from 'vue';
+import './globals.css';
+import App from './App.vue';
 
 createApp(App).mount('#app');
