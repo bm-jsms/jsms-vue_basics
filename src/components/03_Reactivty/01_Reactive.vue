c552c7dece470633fbae487fedda8ff28d8bc86c
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-14 04:09:09 -0500

    03 _ Reactivity

---
+++ src/components/03_Reactivty/01_Reactive.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive } from 'vue';
+
+let initialState = reactive({
+	Val: {
+		count: 0,
+	},
+	user: ['JSMS', 'Jhon'],
+});
+const changeUser = () => {
+	initialState.user[0] = 'Jhon';
+	initialState.user[1] = 'Alex';
+};
+</script>
+<template>
+	<h1>Count: {{ initialState.count }}</h1>
+	<button @click="initialState.count--">Decrement</button>
+	<button @click="initialState.count++">Increment</button>
+
+	<hr />
+
+	<h1>Users: {{ initialState.user }}</h1>
+	<button @click="changeUser">Change User</button>
+</template>
---
+++ src/components/03_Reactivty/02_Ref.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+let count = ref(0);
+
+let username = ref('');
+
+let usersList = ref(['JSMS', 'John', 'Jordan']);
+</script>
+
+<template>
+	<h1>Count: {{ count }}</h1>
+	<button @click="count++">+</button>
+	<button @click="count--">+</button>
+
+	<hr />
+
+	<h1>Username: {{ username }}</h1>
+	<button @click="username = 'Alex'">Add User</button>
+
+	<hr />
+
+	<h1>Users List: {{ usersList }}</h1>
+	<button @click="usersList.push('Matias')">Add User</button>
+</template>
