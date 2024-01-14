2686ac3399fa50bf8e0a519e499a58bb2b351230
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-14 01:58:07 -0500

    00 _ Intro vue 3

---
+++ src/components/00_intro/ChildComponent.vue
@@ -0,0 +1,7 @@
+<template>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
+		distinctio, officiis ex numquam eligendi harum aspernatur mollitia
+		molestiae.
+	</p>
+</template>
---
+++ src/components/01_TextInterpolation/01_intro.vue
@@ -0,0 +1,19 @@
+<script setup>
+const myTitle = 'Text Interpolation';
+const myFn = message => {
+	return message;
+};
+const myNumber = 10;
+</script>
+
+<template>
+	<h1>
+		{{ myTitle }}
+	</h1>
+
+	<p>
+		{{ myFn('This is a function') }}
+	</p>
+
+	<p>My number is: {{ myNumber }}</p>
+</template>
