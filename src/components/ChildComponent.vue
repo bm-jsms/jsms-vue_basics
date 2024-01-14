693d060961e080fdc95c488d8327a5b9e1d5ca74
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-14 00:06:26 -0500

    child component - lorem

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -2,7 +2,6 @@
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
 		distinctio, officiis ex numquam eligendi harum aspernatur mollitia
-		molestiae, quia provident laboriosam? Nulla animi fugiat dolore nihil.
-		Corporis fugiat temporibus aliquam?
+		molestiae.
 	</p>
 </template>
