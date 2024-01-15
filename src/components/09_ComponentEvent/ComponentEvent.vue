baff091f2e4de6dd4c7d1eb3403d3ea61a8ca00b
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-15 04:40:25 -0500

    09 - component event

---
+++ src/components/09_ComponentEvent/ComponentEvent.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+	<button @click="$emit('increment')">+</button>
+</template>
+<style scoped></style>
---
+++ src/components/09_ComponentEvent/main.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+import ComponentEvent from './ComponentEvent.vue';
+
+let count = ref(0);
+</script>
+<template>
+	<h1>Count: {{ count }}</h1>
+	<ComponentEvent @increment="count++" />
+</template>
+<style scoped></style>
