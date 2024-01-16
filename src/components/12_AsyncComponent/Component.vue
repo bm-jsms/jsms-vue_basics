ebc2983230d9f402fbc9b492e2704f8471eb50b2
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-16 07:01:26 -0500

    12 - async component

---
+++ src/components/12_AsyncComponent/Component.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+	<div>
+		<h1>
+            This is the async component
+        </h1>
+	</div>
+</template>
+<style scoped></style>
---
+++ src/components/12_AsyncComponent/main.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineAsyncComponent, ref } from 'vue';
+
+const showComponent = ref(false);
+const renderComponent = defineAsyncComponent(() => import('./Component.vue'));
+</script>
+<template>
+	<div>
+		<h1>Async Component</h1>
+		<p>Click the button to load the component</p>
+		<div v-if="showComponent">
+			<renderComponent />
+		</div>
+		<button @click="showComponent = !showComponent">show | hide</button>
+	</div>
+</template>
+<style scoped></style>
