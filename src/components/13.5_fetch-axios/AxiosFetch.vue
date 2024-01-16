cc67c6bbc483115f00befa78b7e9bfa455b00a1b
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-16 07:27:28 -0500

    13.5 - axios fetch

---
+++ src/components/13.5_fetch-axios/AxiosFetch.vue
@@ -0,0 +1,25 @@
+<script setup>
+import axios from 'axios';
+import { Ref } from 'vue';
+const data = ref(null);
+
+const fetchData = async () => {
+	try {
+		const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+		data.value = res.data;
+	} catch (error) {
+		console.error(error);
+	}
+};
+</script>
+<template>
+	<div>
+		<h1>Using Axios</h1>
+		<button @click="fetchData">Fetch Data</button>
+		<div>
+			<h2>Axios Data</h2>
+			<pre>{{ data }} </pre>
+		</div>
+	</div>
+</template>
+<style scoped></style>
