99415ab12b5801df71376b97a108658d978b22f1
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-16 07:23:24 -0500

    13 - fetch

---
+++ src/components/13_fetch/fetchComponent.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+const data = ref(null);
+
+const fetchData = () => {
+	fetch('https://jsonplaceholder.typicode.com/posts')
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			return response.json();
+		})
+		.then(jsonResponse => {
+			data.value = jsonResponse;
+		})
+		.catch(error => {
+			console.error(
+				'There has been a problem with your fetch operation:',
+				error,
+			);
+		});
+};
+</script>
+<template>
+	<div>
+		<h1>Using Fetch API</h1>
+		<button @click="fetchData">Fetch Data</button>
+		<div>
+			<h2>FetchData</h2>
+			<pre>{{ data }} </pre>
+		</div>
+	</div>
+</template>
+<style scoped></style>
---
+++ src/components/13_fetch/main.vue
@@ -0,0 +1,7 @@
+<script setup>
+import fetchComponent from './fetchComponent.vue';
+</script>
+<template>
+	<fetchComponent />
+</template>
+<style scoped></style>
